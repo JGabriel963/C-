36f130c1a32a25b96c05f04472dc3df3e860deb1
João Gabriel <joaogabriel9633@gmail.com>
2026-05-12 22:47:39 -0300

    Petfolio

---
+++ Solution1/Petfolio.Application/UseCases/Pet/Register/RegisterPetUserCase.cs
@@ -0,0 +1,16 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pet.Register;
+
+public class RegisterPetUserCase
+{
+    public ResponseRegisteredPetJson Execute(RequestRegisterPetJson request)
+    {
+        return new ResponseRegisteredPetJson
+        {
+            Id = 1,
+            Name = request.Name
+        };
+    }
+}
---
+++ Solution1/PetfolioAPI/Controllers/PetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PetController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredPetJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    {
+        var userCase = new RegisterPetUserCase();
+
+        var response = userCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+}
