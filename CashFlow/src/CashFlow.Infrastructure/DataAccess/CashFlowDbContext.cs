9483571c8c4864d995f978e02224160140d24de5
João Gabriel <joaogabriel9633@gmail.com>
2026-05-21 22:13:17 -0300

    Entiti Framework

---
+++ CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext: DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflow;Uid=cashflow;Pwd=cashflow;";
+
+        var version = new Version(8, 4);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
