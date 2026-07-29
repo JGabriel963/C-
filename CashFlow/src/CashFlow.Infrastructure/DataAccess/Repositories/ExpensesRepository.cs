ecfd2c879dc0940fa45630096c6185b8121f27ac
João Gabriel <joaogabriel9633@gmail.com>
2026-07-29 10:39:49 -0300

    feat: enhance expense management with tags support and validation

--- CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
+++ CashFlow/src/CashFlow.Infrastructure/DataAccess/Repositories/ExpensesRepository.cs
@@ -1,6 +1,7 @@
 ﻿using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories.Expenses;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace CashFlow.Infrastructure.DataAccess.Repositories;
 
@@ -33,14 +34,16 @@ internal class ExpensesRepository : IExpensesWriteOnlyRepository, IExpensesReadO
 
     async Task<Expense?> IExpensesReadOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses
+        return await GetFullExpense()
             .AsNoTracking()
             .FirstOrDefaultAsync(expense => expense.Id == id && expense.UserId == user.Id);
     }
 
     async Task<Expense?> IExpensesUpdateOnlyRepository.GetById(User user, long id)
     {
-        return await _dbContext.Expenses.FirstOrDefaultAsync(expense => expense.Id == id && expense.UserId == user.Id);
+        return await GetFullExpense()
+            .Include(expense => expense.Tags)
+            .FirstOrDefaultAsync(expense => expense.Id == id && expense.UserId == user.Id);
     }
 
     public void Update(Expense expense)
@@ -62,5 +65,11 @@ internal class ExpensesRepository : IExpensesWriteOnlyRepository, IExpensesReadO
              .OrderBy(expense => expense.Date)
              .ThenBy(expense => expense.Title)
              .ToListAsync();
+    }
+
+    private IIncludableQueryable<Expense, ICollection<Tag>> GetFullExpense()
+    {
+        return _dbContext.Expenses
+            .Include(expense => expense.Tags);
     }
 }
